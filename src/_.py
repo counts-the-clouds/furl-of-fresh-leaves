0276945f47b70ed2ed515b9602c134b4a2742f28
Counts The Clouds <counts-the-clouds@water-through-fog.com>
2024-07-18 19:15:09 -0500

    Explain myself

--- src/_.py
+++ src/_.py
@@ -1,4 +1,10 @@
 
+# In the main project I'm using the underscore to denote global constants.
+# Appearently that's not really done in Python (I'm not a Python guy obviously)
+# so putting all globals into a file that everything imports seems like the way
+# to go. By naming this module "_" globals can be referenced like _.TILE_SIZE
+# elsewhere.
+
 TILE_SIZE = 128
 
 N = 'N'
